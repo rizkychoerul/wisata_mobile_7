bb4ad5d2cf6deedc03909e736857cd3981d78d74
Rizky <rcf2512@gmail.com>
2024-12-05 20:19:13 +0700

    Perubahan homescreen

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -32,6 +32,13 @@ class _HomescreenState extends State<Homescreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kBackgroundColor,
+      appBar: AppBar(
+        title: Text(
+          'Cibodas App',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: kButtonColor,
+      ),
       body: Column(
         children: [
           buildAppBar(),
@@ -76,7 +83,7 @@ class _HomescreenState extends State<Homescreen> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                  'Tempat Populer',
+                  'Tempat Rekomendasi',
                   style: TextStyle(
                     fontSize: 16,
                     fontWeight: FontWeight.w600,
@@ -112,10 +119,12 @@ class _HomescreenState extends State<Homescreen> {
               mainAxisSize: MainAxisSize.min,
               children: [
                 Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
                   decoration: BoxDecoration(
-                      color: kButtonColor,
-                      borderRadius: BorderRadius.circular(10)),
+                    color: kBackgroundColor,
+                    // borderRadius: BorderRadius.circular(10),
+                  ),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: List.generate(
@@ -130,8 +139,8 @@ class _HomescreenState extends State<Homescreen> {
                           icons[index],
                           size: 32,
                           color: selectedPage == index
-                              ? Colors.white
-                              : Colors.white.withOpacity(0.4),
+                              ? Colors.black
+                              : Colors.black.withOpacity(0.4),
                         ),
                       ),
                     ),
@@ -149,60 +158,71 @@ class _HomescreenState extends State<Homescreen> {
 Widget buildAppBar() {
   return Container(
     height: 120,
+    margin: EdgeInsets.only(left: 10, right: 10, top: 20),
     decoration: BoxDecoration(
-      color: Colors.white,
+      // color: Colors.white,
       borderRadius: const BorderRadius.only(
-        bottomLeft: Radius.circular(25),
-        bottomRight: Radius.circular(25),
+        topLeft: Radius.circular(20),
+        topRight: Radius.circular(20),
+        bottomLeft: Radius.circular(20),
+        bottomRight: Radius.circular(20),
       ),
-      boxShadow: [
-        BoxShadow(
-            color: Colors.grey.withOpacity(0.7),
-            spreadRadius: 5,
-            blurRadius: 10,
-            offset: const Offset(0, 5)),
-      ],
+      // boxShadow: [
+      //   BoxShadow(
+      //       color: Colors.grey.withOpacity(0.7),
+      //       spreadRadius: 5,
+      //       blurRadius: 10,
+      //       offset: const Offset(0, 5)),
+      // ],
     ),
     child: SafeArea(
-        child: Center(
-      child: Padding(
-        padding: const EdgeInsets.only(top: 20, left: 15, right: 15),
-        child: buildSearchButton(),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 15, right: 15),
+          child: Text(
+            'Selamat Datang di Cibodas App',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w800,
+              color: Colors.black,
+            ),
+          ),
+        ),
       ),
-    )),
+    ),
   );
 }
 
-Widget buildSearchButton() {
-  return Container(
-    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-    decoration: BoxDecoration(
-      color: kButtonColor,
-      borderRadius: BorderRadius.circular(20),
-      boxShadow: [
-        BoxShadow(
-          color: Colors.grey.withOpacity(0.8),
-          spreadRadius: 2,
-          blurRadius: 10,
-          offset: const Offset(0, 5),
-        ),
-      ],
-    ),
-    child: const TextField(
-      decoration: InputDecoration(
-        hintText: 'cari destinasi...',
-        hintStyle: TextStyle(
-          color: Colors.white54,
-          fontSize: 18,
-        ),
-        prefixIcon: Icon(Icons.search, color: Colors.white54, size: 28),
-        border: InputBorder.none,
-        contentPadding: EdgeInsets.symmetric(vertical: 12),
-      ),
-      style: TextStyle(
-        color: Colors.white,
-        fontSize: 18,
-      ),
-    ),
-  );
-}
+// Widget buildSearchButton() {
+//   return Container(
+//     padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+//     decoration: BoxDecoration(
+//       color: kButtonColor,
+//       borderRadius: BorderRadius.circular(20),
+//       boxShadow: [
+//         BoxShadow(
+//           color: Colors.grey.withOpacity(0.8),
+//           spreadRadius: 2,
+//           blurRadius: 10,
+//           offset: const Offset(0, 5),
+//         ),
+//       ],
+//     ),
+//     child: const TextField(
+//       decoration: InputDecoration(
+//         hintText: 'cari destinasi...',
+//         hintStyle: TextStyle(
+//           color: Colors.white54,
+//           fontSize: 18,
+//         ),
+//         prefixIcon: Icon(Icons.search, color: Colors.white54, size: 28),
+//         border: InputBorder.none,
+//         contentPadding: EdgeInsets.symmetric(vertical: 12),
+//       ),
+//       style: TextStyle(
+//         color: Colors.white,
+//         fontSize: 18,
+//       ),
+//     ),
+//   );
+// }
